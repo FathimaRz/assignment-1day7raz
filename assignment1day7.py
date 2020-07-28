78fd265be13095a4565c357b4f6b0c27a948d7d2
FathimaRz <68552053+FathimaRz@users.noreply.github.com>
2020-07-28 19:23:59 +0530

    Add files via upload

---
+++ assignment1day7.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+port1={21:"FTP",22:"SSH",23:"telnet",80:"http"}
+print(port1)
+
+
+# In[6]:
+
+
+port1={value:key for key, value in port1.items()}
+print(port1)
+
+
+# In[ ]:
+
+
+
+
